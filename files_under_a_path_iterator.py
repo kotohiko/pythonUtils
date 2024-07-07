e3fc26e41e67a1d65a0716998d342c5d28efdf12
JacobSuen <tachibana.kotohiko@gmail.com>
2024-07-07 11:54:59 +0800

    初始化

---
+++ files_under_a_path_iterator.py
@@ -0,0 +1,22 @@
+import os
+
+
+def list_files(startpath):
+    files_counts = 0
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        # 在py中，缩进真乃善用之物
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+            files_counts = files_counts + 1
+    print("The Total number of files: {}".format(files_counts))
+
+
+startpath = 'S:\Gallery\【二次元】Anime World／二次元世界／アニメの世界'
+list_files(startpath)
+
+if __name__ == '__main__':
+    list_files('PyCharm')
